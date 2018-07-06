01fa1530ae7f2c74f352316eed726e893d9c4a9f
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2018-07-06 13:48:32 +0100

    EE-4476 Create a basic test for IPS search screen

---
+++ src/main/java/uk/gov/digital/ho/proving/income/IpsSearchPage.java
@@ -0,0 +1,69 @@
+package uk.gov.digital.ho.proving.income;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class IpsSearchPage {
+
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    @FindBy(id = "forename")
+    private WebElement forename;
+
+    @FindBy(id = "surname")
+    private WebElement surname;
+
+    @FindBy(id = "dateOfBirthDay")
+    private WebElement dateOfBirthDay;
+
+    @FindBy(id = "dateOfBirthMonth")
+    private WebElement dateOfBirthMonth;
+
+    @FindBy(id = "dateOfBirthYear")
+    private WebElement dateOfBirthYear;
+
+    @FindBy(id = "nino")
+    private WebElement nino;
+
+    @FindBy(id = "dependants")
+    private WebElement dependants;
+
+    @FindBy(id = "applicationRaisedDateDay")
+    private WebElement applicationRaisedDateDay;
+
+    @FindBy(id = "applicationRaisedDateMonth")
+    private WebElement applicationRaisedDateMonth;
+
+    @FindBy(id = "applicationRaisedDateYear")
+    private WebElement applicationRaisedDateYear;
+
+    @FindBy(id = "searchBtn")
+    private WebElement searchButton;
+
+    @FindBy(id = "validation-error-summary-heading")
+    private WebElement errorSummaryHeader;
+
+    public IpsSearchPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        this.wait = new WebDriverWait(driver, 30);
+    }
+
+    public void search() {
+        this.searchButton.click();
+    }
+
+    public WebElement getErrorSummaryHeader() {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("validation-error-summary-heading")));
+        return errorSummaryHeader;
+    }
+
+
+
+}
---
+++ src/test/java/uk/gov/digital/ho/proving/income/IpsSearchTest.java
@@ -0,0 +1,41 @@
+package uk.gov.digital.ho.proving.income;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class IpsSearchTest {
+
+    private WebDriver webDriver;
+    private IpsSearchPage ipsSearchPage;
+
+    @BeforeClass
+    public static void setUpClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Before
+    public void setUpTest() {
+        webDriver = new ChromeDriver();
+        ipsSearchPage = new IpsSearchPage(webDriver);
+
+    }
+
+    @Test
+    public void thatInvalidSearchesShowErrors() {
+        ipsSearchPage.search();
+        assertThat(ipsSearchPage.getErrorSummaryHeader()).isNotNull().withFailMessage("The error summary should be displayed");
+    }
+
+    @After
+    public void tearDown()  {
+        webDriver.quit();
+    }
+
+}
